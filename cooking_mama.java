dd3c5073d6bbc9f141eb83920bb522776f716b21
Hefot Ahmed <hefotahmed@Hefots-MacBook-Air.local>
2025-12-03 13:01:16 -0500

    Initial  commit

---
+++ cooking_mama.java
@@ -0,0 +1,7 @@
+public class cooking_mama {
+    public static void main(String[] args) {
+        System.out.println("Step 1: Toast the buns");
+        System.out.println("Step 2: Bread your chicken");
+        System.out.println("Step 3: Chop the tomatoes");
+    }
+}
