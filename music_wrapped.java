c2cd51d20426cc5ae3dbd6ec7b16c244126e3942
Hefot Ahmed <hefot6@gmail.com>
2025-12-04 15:34:12 -0500

    Add HelloWorld class to display music list

---
+++ music_wrapped.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("========== 🎵 Music Wrapped 🎵 ==========");
+        
+        System.out.println("Shape of you"); // Ed Sheeran
+        System.out.println("Attention"); // Charlie Puth 
+        System.out.println("Despacito"); // Justin Bieber 
+        System.out.println("Wildflower"); // Billie Eilish 
+        System.out.println("Diamonds"); // Rihanna         
+    }
+}
