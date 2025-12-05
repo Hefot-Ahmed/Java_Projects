35d4c9d5717b17b5a5fbb91de1ff55b766782bc3
Hefot Ahmed <hefot6@gmail.com>
2025-12-05 16:55:18 -0500

    Add HelloWorld class to calculate reaction result

---
+++ operation_0.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+    double chemicalA = 50.25;
+    double chemicalB = 25.75;
+    double reactionResult = (chemicalA + chemicalB) / (chemicalA * chemicalB);
+
+    System.out.println(reactionResult);
+
+    }
+}
