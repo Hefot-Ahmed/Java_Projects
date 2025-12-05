6dc325189965e1e8f3fa4f06d552453646cf56fc
Hefot Ahmed <hefot6@gmail.com>
2025-12-05 15:30:30 -0500

    Add HelloWorld class with personal information display

---
+++ id_card.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+    String name = "Hefot";
+    String state = "Ontario";
+    char gender = 'M';
+    int age = 24;
+    boolean organDonor = false;
+
+    System.out.println(name);
+    System.out.println(state);
+    System.out.println(gender);
+    System.out.println(age);
+    System.out.println(organDonor);
+
+    }
+}
