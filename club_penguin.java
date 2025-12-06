5866bad38eab66dfdd20aa0145577dd6f9508e0c
Hefot Ahmed <hefot6@gmail.com>
2025-12-06 12:20:00 -0500

    Add Club Penguin Money Converter program

---
+++ club_penguin.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+    double exchangeRate = 0.0045;
+
+    System.out.print("Welcome to the Club Penguin Money Converter!");
+    System.out.print("How many coins do you have?");
+    int coins = scanner.nextInt(); 
+
+    double dollars = coins * exchangeRate;
+
+    System.out.print("You have the following amount of dollars:");
+    System.out.println(dollars);
+    }
+} 
